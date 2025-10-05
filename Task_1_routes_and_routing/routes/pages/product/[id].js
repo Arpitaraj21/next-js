dbc64839c3b2c1fa188351336e80e86b5bf470b8
Arpita Raj <singh.arpitaraj2001@gmail.com>
2025-10-05 18:28:46 +0530

    static and dynamic route in pages router

---
+++ Task_1_routes_and_routing/routes/pages/product/[id].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export default function Product(){
+    const router = useRouter();
+    const {id} = router.query;
+    return(
+        <>
+        id is {id}
+        </>
+    )
+}
