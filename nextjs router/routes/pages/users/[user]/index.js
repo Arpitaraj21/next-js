22b577d35b4312a87e410f69ca4dc49d1113b4e1
Arpita Raj <singh.arpitaraj2001@gmail.com>
2025-10-07 22:14:04 +0530

    replace and push

--- nextjs router/routes/pages/users/[user]/index.js
+++ nextjs router/routes/pages/users/[user]/index.js
@@ -1,7 +1,23 @@
 import { useRouter } from "next/router";
 
+import { useEffect } from "react";
+
+const useUser = () => ({
+    user: null,
+    loading: false
+});
 const functionPage = () => {
     const router = useRouter();
+    const user = useUser();
+
+    console.log(router);
+
+    useEffect(() => {
+        if(user.user == null){
+            router.replace('/');
+        }
+    },[router, user.user]);
+
     return(
         <>
         <h1>This is index page for {router.query.user}</h1>
