21096204018f1347e23e36d0438f3c95c1b08338
Arpita Raj <singh.arpitaraj2001@gmail.com>
2025-10-09 21:19:34 +0530

    server side rendering

---
+++ Server Side Rendering/practice/pages/server/users.jsx
@@ -0,0 +1,21 @@
+const UserPage = (props) => {
+    console.log(props);
+    return(
+        <div>
+            <p>this is a SSR</p>
+            {props.data.users.map(user => <li key={user.id}>{user.firstName}</li>)}
+        </div>
+    )
+}
+
+export const getServerSideProps = async () => {
+    console.log('A task is going on in server');
+    const data = await (await fetch('https://dummyjson.com/users')).json()
+    return {
+        props: {
+            data,
+        }
+    }
+}
+
+export default UserPage;
