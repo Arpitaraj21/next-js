30207fe5b7e53288e2e19fb0b256d43c8dc923fe
Arpita Raj <singh.arpitaraj2001@gmail.com>
2025-10-14 19:46:57 +0530

    inital commit for authentication in next js

--- Server Side Generation/practice/pages/server/users.jsx
+++ Server Side Generation/practice/pages/server/users.jsx
@@ -1,11 +1,21 @@
 import styles from '../../styles/Users.module.css'
+import Image from 'next/image';
+import codeImage from '../../public/image.png'
 const UserPage = (props) => {
+
+    const myImageLoader = ({src, width, quality}) => {
+        return `https://images.pixel.com/photos/270348/pexels-photo-270349.jpeg?auto=compress&cs=tinysgb&w=${width}&h=${height}&dpr=1`
+    }
     console.log(props);
     return(
         <div className={styles.p}>
             <p >this is a SSR</p>
             {props.data.users.map(user => <li key={user.id}>{user.firstName}</li>)}
+            <Image
+            loader={myImageLoader}
+             src={codeImage} alt="Code Image" width={300} height={300}/>
         </div>
+
     )
 }
 
